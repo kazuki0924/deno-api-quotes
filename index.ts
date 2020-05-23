ee367a7294f98b321204c6e6d179cb327803c6fd
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-23 15:39:24 +0900

    1

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
-import { serve } from 'https://deno.land/std@0.52.0/http/server.ts';
-const s = serve({ port: 8000 });
-console.log('http://localhost:8000/');
-for await (const req of s) {
-	req.respond({ body: 'Hello World\n' });
-}
+import {
+	Application,
+	Router
+} from "https://deno.land/x/denotrain@v0.5.0/mod.ts";
+import { quotes } from "./quotes.ts";
+
+// Create a new application (port defaults to 3000, hostname to 0.0.0.0)
+const app = new Application({});
+// Optional: Generate router and hook routes to it
+const router = new Router();
+
+app.use("/api", router)
+
+
+app.get("/", (ctx) => {
+	return { hello: "world" };
+});
+
+router.get("/quotes", (ctx) => {
+	return { "quotes": quotes };
+});
+
+await app.run();
